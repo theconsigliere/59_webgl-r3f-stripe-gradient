234bf2819dd70df2af4ddb8912b505c908716138
Dirty Martini <hello@maxwellkirwin.co.uk>
2024-06-23 18:29:27 +0100

    done

--- app/components/Plane.jsx
+++ app/components/Plane.jsx
@@ -1,6 +1,7 @@
 import { useRef } from "react"
 import { useThree, useFrame } from "@react-three/fiber"
 import { useControls } from "leva"
+import * as THREE from "three"
 
 import { NewShaderMaterial } from "./Shader"
 
@@ -11,11 +12,14 @@ export default function Plane() {
   const shaderProps = useControls("Shader Props", {
     //   rings: { value: 10, min: 1, max: 100 },
     //  fract: { value: 2.0, min: 0.1, max: 10 },
-    color1: "#ff0000",
+    color1: "#00FFFF",
+    color2: "#0099f2",
+    color3: "#4b55de",
+    color4: "#1ef2b2",
+    color5: "#caebe9",
+    frequency: [0.2, 0.25],
     wireframe: false,
-    // color2: "#00ff00",
-    // color3: "#0000ff",
-    // color4: "#ff00ff",
+    time: { value: 0.1, min: 0.001, max: 1.0 },
   })
 
   useFrame((state, delta) => {
@@ -23,18 +27,23 @@ export default function Plane() {
   })
 
   return (
-    <mesh scale={[viewport.width, viewport.height, 1]}>
+    <mesh scale={[viewport.width * 2, viewport.height * 2, 1]}>
       {/*  */}
-      <planeGeometry args={[1, 1, 300, 300]} />
+      <planeGeometry args={[1, 1, 100, 100]} />
       <newShaderMaterial
         ref={ref}
         key={NewShaderMaterial.key}
         uResolution={[size.width * viewport.dpr, size.height * viewport.dpr]}
-        uColor1={shaderProps.color1}
         wireframe={shaderProps.wireframe}
-        // uColor2={shaderProps.color2}
-        // uColor3={shaderProps.color3}
-        // uColor4={shaderProps.color4}
+        uColors={[
+          new THREE.Color(shaderProps.color1),
+          new THREE.Color(shaderProps.color2),
+          new THREE.Color(shaderProps.color3),
+          new THREE.Color(shaderProps.color4),
+          new THREE.Color(shaderProps.color5),
+        ]}
+        uFreq={shaderProps.frequency}
+        uMultiplyTime={shaderProps.time}
       />
     </mesh>
   )
--- app/components/Shader.jsx
+++ app/components/Shader.jsx
@@ -10,24 +10,36 @@ const NewShaderMaterial = shaderMaterial(
   {
     uTime: 0,
     uResolution: new THREE.Vector2(),
-    uColor1: new THREE.Color("#000000"),
-    // uColor2: new THREE.Color("#00ff00"),
-    // uColor3: new THREE.Color("#0000ff"),
-    // uColor4: new THREE.Color("#ff00ff"),
+    uColors: [
+      new THREE.Color("#00FFFF"),
+      new THREE.Color("#0099f2"),
+      new THREE.Color("#4b55de"),
+      new THREE.Color("#1ef2b2"),
+      new THREE.Color("#caebe9"),
+    ],
+    uFreq: [0.3, 0.4],
+    uMultiplyTime: 0.1,
     side: THREE.DoubleSide,
   },
   // THREE.doubleSide renders both sides of the plane
 
-  /*vertex*/ `      
+  /*vertex*/ `   
+      uniform float uTime;  
+      uniform vec3 uColors[5];
+      uniform vec2 uFreq;
+      uniform float uMultiplyTime;
+
       varying vec2 vUv;
-      uniform float uTime;
-  
+      varying vec3 vColor;
+   
       ${noise}
 
       void main()
       {
         vUv = uv;
+        float time = uTime * uMultiplyTime;
         vec2 noiseCoord = uv*vec2(3., 4.);
+
 
         // tilt vertexs away from camera
         float tilt = -0.8*vUv.y;
@@ -37,32 +49,63 @@ const NewShaderMaterial = shaderMaterial(
         
         float offset = incline * mix(-.25,.25,vUv.y);
 
-
-        float noise = snoise(vec3(noiseCoord.x + uTime *3., noiseCoord.y, uTime * 10.));
+        float noise = snoise(vec3(noiseCoord.x + time *3., noiseCoord.y, time * 10.));
 
         // stop noise from going below 0 keeps plane still plane like
         noise = max(0., noise);
 
         vec3 newPosition = vec3(position.xy, position.z + noise * .75 + tilt + incline + offset);
 
+
         //Position
         vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
         // Final position
         gl_Position = projectionMatrix * viewMatrix * modelPosition;
-        vUv = uv;
+      
+        
+        // Colors
+        vColor = uColors[4];
+
+        for(int i = 0; i < 5; i++)
+        {
+
+        float noiseFlow = 5. + float(i) * .3;
+        float noiseSpeed = 10. + float(i) * .3;
+        float noiseSeed = 1. + float(i) * 10.;
+
+       // float noiseFloor = .1;
+      //  float noiseCeil = .6 + float(i) +0.07;
+     
+        float noise = 
+       // smoothstep(noiseFloor, noiseCeil,
+          snoise(
+              vec3(noiseCoord.x * uFreq.x + time * noiseFlow, 
+              noiseCoord.y * uFreq.y, 
+              time * noiseSpeed + noiseSeed
+            )
+         // )
+        );
+
+
+        vColor = mix(vColor, uColors[i], noise);
+         
+        }
+
+
+
       }
       `,
   /*fragment*/ `
- 
-      uniform vec3 uColor1;
+
       varying vec2 vUv;  
-   
-      
+      varying vec3 vColor;
+
+    
       void main()
       {
+
      
-      //  gl_FragColor = vec4(uColor1, 1.0);
-       gl_FragColor = vec4(vUv, 1., 1.0);
+        gl_FragColor = vec4(vColor, 1.0);
       }`
 )
 
