ccc5e5fe49183cae51635081c700c8b72cf644a3
Dirty Martini <hello@maxwellkirwin.co.uk>
2024-06-23 17:18:58 +0100

    halfway

--- app/components/Plane.jsx
+++ app/components/Plane.jsx
@@ -8,27 +8,30 @@ export default function Plane() {
   const ref = useRef()
   const { viewport, size } = useThree()
 
-  // const shaderProps = useControls("Shader Props", {
-  //   rings: { value: 10, min: 1, max: 100 },
-  //  fract: { value: 2.0, min: 0.1, max: 10 },
-  // color1: "#ff0000",
-  // color2: "#00ff00",
-  // color3: "#0000ff",
-  // color4: "#ff00ff",
-  // })
+  const shaderProps = useControls("Shader Props", {
+    //   rings: { value: 10, min: 1, max: 100 },
+    //  fract: { value: 2.0, min: 0.1, max: 10 },
+    color1: "#ff0000",
+    wireframe: false,
+    // color2: "#00ff00",
+    // color3: "#0000ff",
+    // color4: "#ff00ff",
+  })
 
   useFrame((state, delta) => {
-    ref.current.uTime += delta
+    ref.current.uTime += delta * 0.05
   })
 
   return (
     <mesh scale={[viewport.width, viewport.height, 1]}>
-      <planeGeometry />
+      {/*  */}
+      <planeGeometry args={[1, 1, 300, 300]} />
       <newShaderMaterial
         ref={ref}
         key={NewShaderMaterial.key}
         uResolution={[size.width * viewport.dpr, size.height * viewport.dpr]}
-        // uColor1={shaderProps.color1}
+        uColor1={shaderProps.color1}
+        wireframe={shaderProps.wireframe}
         // uColor2={shaderProps.color2}
         // uColor3={shaderProps.color3}
         // uColor4={shaderProps.color4}
--- app/components/Shader.jsx
+++ app/components/Shader.jsx
@@ -1,13 +1,16 @@
 import * as THREE from "three"
 import { extend } from "@react-three/fiber"
 import { shaderMaterial } from "@react-three/drei"
+import { noise } from "./shaders/noise"
+
+// remove json formating from noise
 
 // Tutorial: https://www.youtube.com/watch?v=f4s1h2YETNY
 const NewShaderMaterial = shaderMaterial(
   {
     uTime: 0,
     uResolution: new THREE.Vector2(),
-    // uColor1: new THREE.Color("#ff0000"),
+    uColor1: new THREE.Color("#000000"),
     // uColor2: new THREE.Color("#00ff00"),
     // uColor3: new THREE.Color("#0000ff"),
     // uColor4: new THREE.Color("#ff00ff"),
@@ -17,25 +20,49 @@ const NewShaderMaterial = shaderMaterial(
 
   /*vertex*/ `      
       varying vec2 vUv;
+      uniform float uTime;
+  
+      ${noise}
+
       void main()
       {
+        vUv = uv;
+        vec2 noiseCoord = uv*vec2(3., 4.);
+
+        // tilt vertexs away from camera
+        float tilt = -0.8*vUv.y;
+
+        // inclione palne towards camera
+        float incline = vUv.x*0.1;
+        
+        float offset = incline * mix(-.25,.25,vUv.y);
+
+
+        float noise = snoise(vec3(noiseCoord.x + uTime *3., noiseCoord.y, uTime * 10.));
+
+        // stop noise from going below 0 keeps plane still plane like
+        noise = max(0., noise);
+
+        vec3 newPosition = vec3(position.xy, position.z + noise * .75 + tilt + incline + offset);
+
         //Position
-        vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+        vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
         // Final position
         gl_Position = projectionMatrix * viewMatrix * modelPosition;
         vUv = uv;
       }
       `,
   /*fragment*/ `
-      uniform float uTime;
-
+ 
+      uniform vec3 uColor1;
       varying vec2 vUv;  
+   
       
-       
       void main()
       {
      
-       gl_FragColor = vec4(vUv, 1.0, 1.0);
+      //  gl_FragColor = vec4(uColor1, 1.0);
+       gl_FragColor = vec4(vUv, 1., 1.0);
       }`
 )
 
